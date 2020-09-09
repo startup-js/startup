4d041b4223233f5933f7606ecdfeb4fd346b9d61
Marcus Ataide <marckaraujo86@gmail.com>
2020-09-09 14:40:09 -0300

    Fix layout break

--- layouts/default/index.js
+++ layouts/default/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 import './global.scss';
 import { firePageView } from 'lib/gtag';
+import { SponsorBanner } from '../../components/sponsor-banner';
 
 class DefaultLayout extends React.Component {
   state = {
@@ -15,6 +16,9 @@ class DefaultLayout extends React.Component {
   render() {
     return (
       <div>
+        {/*<div className={ classNames('banner-wrap', { 'd-none': this.state.isBannerClosed }) }>*/}
+        {/*  <SponsorBanner onCloseBanner={() => this.setState({ isBannerClosed: true })} />*/}
+        {/*</div>*/}
         <div className={ classNames('body-wrap', { 'sponsor-banner-visible': !this.state.isBannerClosed }) }>
           { this.props.children }
         </div>
